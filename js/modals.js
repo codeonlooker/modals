2e3720716d20c25f0c564a59723430a1877b04d0
Greg <gregborchard@gmail.com>
2018-04-03 18:24:37 +1000

    Add files via upload

--- js/modals.js
+++ js/modals.js
@@ -1,8 +1,12 @@
 document.querySelector('#openModal').addEventListener('click', function () {
 			document.querySelector('.main').style.opacity = 0;
+			document.querySelector('.main').style.display = 'none';
+			document.querySelector('.pageGridArea').style.backgroundColor = '#e0eeee';
 			document.querySelector('.modals').style.opacity = 1;
 		});
 		document.querySelector('.closeModal').addEventListener('click', function () {
 			document.querySelector('.main').style.opacity = 1;
 			document.querySelector('.modals').style.opacity = 0;
+			document.querySelector('.main').style.display = 'block';
+			document.querySelector('.pageGridArea').style.backgroundColor = '#fff';
 		});
